8de3984c122bc4faedb096802caea714469a603f
David Czarnecki <dczarnecki@agoragames.com>
2012-03-29 15:46:04 -0400

    Bump version back while we get some specs up in hurrrr

--- spec/coffee_bean/version_spec.rb
+++ spec/coffee_bean/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'CoffeeBean::VERSION' do
   it 'should be the correct version' do
-    CoffeeBean::VERSION.should == '1.0.0'
+    CoffeeBean::VERSION.should == '0.1.0'
   end
 end
