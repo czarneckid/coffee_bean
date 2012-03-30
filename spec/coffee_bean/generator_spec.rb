565fc90c2b53bc2c1a6f089d98ce9216e05e0bac
David Czarnecki <czarneckid@acm.org>
2012-03-29 21:51:06 -0400

    Adding spec to make sure we can generate a new CoffeeScript project with option changes

--- spec/coffee_bean/generator_spec.rb
+++ spec/coffee_bean/generator_spec.rb
@@ -18,7 +18,7 @@ describe CoffeeBean::Generator do
   end
 
   describe '#new' do
-    it 'should create the right files when generating a new CoffeeScript project' do
+    it 'should create the right files when generating a new CoffeeScript project with defaults' do
       generator_result = capture(:stdout) do
         CoffeeBean::Generator.start(['new', 'beanery'])
       end
@@ -35,6 +35,26 @@ describe CoffeeBean::Generator do
       File.exist?(File.join(beanery_directory, 'src', 'index.coffee')).should be_true
       File.exist?(File.join(beanery_directory, 'test', 'beanery_test.coffee')).should be_true
       File.exist?(File.join(beanery_directory, 'test', 'test_helper.js')).should be_true
+      FileUtils.rm_rf(beanery_directory)
+    end
+
+    it 'should create the right files when generating a new CoffeeScript project with option changes' do
+      generator_result = capture(:stdout) do
+        CoffeeBean::Generator.start(['new', 'beanery', '--test_framework=spec', '--initial_file=beanery'])
+      end
+
+      beanery_directory = File.dirname(__FILE__) + '/../../beanery'
+      File.directory?(beanery_directory).should be_true
+      File.exist?(File.join(beanery_directory, '.gitignore')).should be_true
+      File.exist?(File.join(beanery_directory, '.npmignore')).should be_true
+      File.exist?(File.join(beanery_directory, 'CHANGELOG.md')).should be_true
+      File.exist?(File.join(beanery_directory, 'LICENSE.txt')).should be_true
+      File.exist?(File.join(beanery_directory, 'Makefile')).should be_true
+      File.exist?(File.join(beanery_directory, 'package.json')).should be_true
+      File.exist?(File.join(beanery_directory, 'README.md')).should be_true
+      File.exist?(File.join(beanery_directory, 'src', 'beanery.coffee')).should be_true
+      File.exist?(File.join(beanery_directory, 'spec', 'beanery_spec.coffee')).should be_true
+      File.exist?(File.join(beanery_directory, 'spec', 'spec_helper.js')).should be_true
       FileUtils.rm_rf(beanery_directory)
     end
   end
